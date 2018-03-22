7f2f44127931f8092024a71b649d4a1b5bbf50f8
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-03-22 18:30:32 +0300

    init

---
+++ src/main/java/com/github/alexivchenko/spring/uaa/client/SpringUaaClientStarterApplication.java
@@ -0,0 +1,11 @@
+package com.github.alexivchenko.spring.uaa.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUaaClientStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUaaClientStarterApplication.class, args);
+    }
+}
