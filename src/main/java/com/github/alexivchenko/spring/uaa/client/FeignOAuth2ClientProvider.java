80ffe69e068b034e861e276f2c5e0bd80b1cd3bf
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2018-04-01 18:31:29 +0300

    update dependencies

--- src/main/java/com/github/alexivchenko/spring/uaa/client/FeignOAuth2ClientProvider.java
+++ src/main/java/com/github/alexivchenko/spring/uaa/client/FeignOAuth2ClientProvider.java
@@ -2,8 +2,8 @@ package com.github.alexivchenko.spring.uaa.client;
 
 import feign.RequestInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
